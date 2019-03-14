42b6259e8386764c9eb9a69fe7e2a8e71cf0affa
joshseyda <josh.seyda@gmail.com>
2019-03-13 20:03:06 -0400

    Testing create/post on posts passes

--- TestDrivenDevelopment/test/integration/post_flow_test.rb
+++ TestDrivenDevelopment/test/integration/post_flow_test.rb
@@ -5,4 +5,15 @@ class PostFlowTest < ActionDispatch::IntegrationTest
     get "/" 
     assert_select "h1", "Post#index"
   end
+  test "can create a post" do
+    get '/posts/new'
+    assert_response :success
+    
+    post "/posts",
+      params: {post: {title: "Post Title", body: "Post body"}}
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "h2", "Post Title"
+  end
 end
